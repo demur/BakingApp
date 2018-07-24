a102d5020fe9a91f3f8cdc7eaeca1fc1a3235992
demur <demur2000@mail.ru>
2018-07-24 17:37:57 -0400

    Fixing Espresso UI testing

--- app/src/androidTest/java/com/udacity/demur/bakingapp/IdlingResourceRecipeListActivityTest.java
+++ app/src/androidTest/java/com/udacity/demur/bakingapp/IdlingResourceRecipeListActivityTest.java
@@ -3,19 +3,14 @@ package com.udacity.demur.bakingapp;
 
 import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResource;
-import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.junit.After;
-import org.junit.Before;
+import com.jakewharton.espresso.OkHttp3IdlingResource;
+import com.udacity.demur.bakingapp.service.RetrofitClient;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,13 +18,12 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.startsWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -38,69 +32,31 @@ public class IdlingResourceRecipeListActivityTest {
     @Rule
     public ActivityTestRule<RecipeListActivity> mActivityTestRule = new ActivityTestRule<>(RecipeListActivity.class);
 
-    private IdlingResource mIdlingResource;
-
-    @Before
-    public void registerIdlingResource() {
-        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
-        IdlingRegistry.getInstance().register(mIdlingResource);
-    }
-
     @Test
     public void recipeListActivityTest() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.rv_recipe_list),
-                        childAtPosition(
-                                withClassName(is("android.support.constraint.ConstraintLayout")),
-                                0)));
-        recyclerView.perform(actionOnItemAtPosition(2, click()));
+        IdlingResource idlingResource = OkHttp3IdlingResource.create(
+                "retrofit_client", RetrofitClient.getOkHttpInstance());
+        IdlingRegistry.getInstance().register(idlingResource);
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        onView(withId(R.id.rv_recipe_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
 
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.tv_step_name), withText("Recipe Introduction"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.rv_recipe_details),
-                                        1),
-                                0),
-                        isDisplayed()));
-        textView.check(matches(withText("Recipe Introduction")));
+        onView(withId(R.id.rv_recipe_details))
+                .check(matches(hasDescendant(withText(containsString("Ingredients")))));
 
+        // In case first item on RecyclerView is "enormous" we need to scroll to desired position to get it on the screen thus load
+        onView(withId(R.id.rv_recipe_details)).perform(scrollToPosition(3));
+
+        onView(withRecyclerView(R.id.rv_recipe_details).atPosition(2))
+                .check(matches(hasDescendant(withText(startsWith("1. ")))));
+
+        onView(withRecyclerView(R.id.rv_recipe_details).atPosition(3))
+                .check(matches(hasDescendant(withText(containsString("2")))));
+
+        IdlingRegistry.getInstance().unregister(idlingResource);
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
-    @After
-    public void unregisterIdlingResource() {
-        if (mIdlingResource != null) {
-            IdlingRegistry.getInstance().unregister(mIdlingResource);
-        }
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
     }
 }
--- app/src/main/java/com/udacity/demur/bakingapp/service/RetrofitClient.java
+++ app/src/main/java/com/udacity/demur/bakingapp/service/RetrofitClient.java
@@ -16,28 +16,36 @@ public class RetrofitClient {
     private static final String TAG = RetrofitClient.class.getSimpleName();
     private static final Object LOCK = new Object();
     private static BakingJsonClient sInstance;
+    private static final Object OKHTTP_LOCK = new Object();
+    private static OkHttpClient sOkHttpInstance;
 
     public static BakingJsonClient getInstance(Context context) {
         if (sInstance == null) {
             synchronized (LOCK) {
                 Log.d(TAG, "Creating new RetrofitClient instance");
-                try
-                {
+                try {
                     URL url = new URL(context.getResources().getString(R.string.json_recipe_source));
                     String baseUrl = url.getProtocol() + "://" + url.getHost();
                     sInstance = new Retrofit.Builder()
                             .baseUrl(baseUrl)
                             .addConverterFactory(GsonConverterFactory.create())
-                            .client(new OkHttpClient.Builder().build())
+                            .client(getOkHttpInstance())
                             .build().create(BakingJsonClient.class);
-                }
-                catch (MalformedURLException e)
-                {
+                } catch (MalformedURLException e) {
                     Log.e(TAG, "getInstance: encountered problem with URL for recipes data");
                 }
             }
         }
         Log.d(TAG, "Getting the RetrofitClient instance");
         return sInstance;
+    }
+
+    public static OkHttpClient getOkHttpInstance() {
+        if (null == sOkHttpInstance) {
+            synchronized (OKHTTP_LOCK) {
+                sOkHttpInstance = new OkHttpClient.Builder().build();
+            }
+        }
+        return sOkHttpInstance;
     }
 }
